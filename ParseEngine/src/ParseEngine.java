d5567f4612037a813edd25e6fb76fa919ea2b94c
Sajjad <Sajjad@Sajjad-PC>
2016-01-01 17:41:34 +0330

    Project classes added

---
+++ ParseEngine/src/ParseEngine.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parseengine;
+
+/**
+ *
+ * @author Administrator
+ */
+public class ParseEngine {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        //VarTer b=new VarTer("s",);
+        // TODO code application logic here
+    }
+    
+}
---
+++ ParseEngine/src/VarTer.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parseengine;
+
+/**
+ *
+ * @author Administrator
+ */
+
+public class VarTer {
+
+    /**
+     * @return the str
+     */
+    public String getStr() {
+        return str;
+    }
+
+    /**
+     * @param str the str to set
+     */
+    public void setStr(String str) {
+        this.str = str;
+    }
+
+    /**
+     * @return the tType
+     */
+    public Tokentypes gettType() {
+        return tType;
+    }
+
+    /**
+     * @param tType the tType to set
+     */
+    public void settType(Tokentypes tType) {
+        this.tType = tType;
+    }
+   public  enum Tokentypes{
+    variable,terminal
+}
+    private String str;
+    private Tokentypes tType;
+    public  VarTer(String str,Tokentypes tokenType){
+        this.str=str;
+        this.tType=tokenType;
+       }
+    }
+
